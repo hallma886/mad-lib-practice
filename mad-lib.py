d8507d367b3df59fe7d78eb87b73b9ff9a72fd20
hallma886 <hallma886@northwested.org>
2024-09-16 13:54:18 -0400

    Add files via upload

---
+++ mad-lib.py
@@ -0,0 +1,10 @@
+noun = input('give me a noun\n')
+verb = input('give me a verb\n')
+adjictive = input('give me a adjictive\n')
+noun2 = input('give me a noun\n')
+noun3 = input('give me a noun\n')
+
+
+mad_lib = "hello I am a " + noun +" I just went for a "+ verb +" I have a friendly "+ noun2 +" who is very "+ adjictive +" and I have a "+ noun3 +"."
+
+print(mad_lib)
